9af62cbac788cb1139e181b08e8a7294ec401120
momoirodouhu <momoirodouhu@gmail.com>
2026-02-28 19:04:01 +0000

    disable HTST

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -7,7 +7,6 @@ func securityHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "DENY")
-		w.Header().Set("Strict-Transport-Security", "max-age=63072000")
 		w.Header().Set("Cross-Origin-Resource-Policy", "same-site")
 		w.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
 		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
